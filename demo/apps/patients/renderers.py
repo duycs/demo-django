bf951ab137219e4e5e6f40ca009d0f75fb8a00a0
duycs <duycs@outlook.com>
2020-02-10 00:38:51 +0700

    remove some model

--- demo/apps/patients/renderers.py
+++
@@ -1,7 +0,0 @@
-from demo.apps.core.renderers import demoJSONRenderer
-
-
-class PatientJSONRenderer(demoJSONRenderer):
-    object_label = 'patient'
-    pagination_object_label = 'patients'
-    pagination_count_label = 'patientsCount'
--- demo/apps/receiptions/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RereceiptionsConfig(AppConfig):
-    name = 'receiptions'
